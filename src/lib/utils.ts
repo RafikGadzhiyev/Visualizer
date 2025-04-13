3f6255365bcfc7ff5e685621b5b79e6b9d6b4729
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-04-13 22:53:39 +0500

    feat(utils): add getMaxNumber and getMinNumber utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,31 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getRandomNumber(from: number, to: number) {
+  return Math.round(Math.random() * (to - from) + from)
+}
+
+export function getMaxNumber(nums: number[]) {
+  let max = -Infinity;
+
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] > max) {
+      max = nums[i]
+    }
+  }
+
+  return max;
+}
+
+export function getMinNumber(nums: number[]) {
+  let max = Infinity;
+
+  for (let i = 0; i < nums.length; ++i) {
+    if (nums[i] < max) {
+      max = nums[i]
+    }
+  }
+
+  return max;
+}
