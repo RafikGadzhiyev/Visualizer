d551ee8823f61dd5e215ce4850260f50beb554f9
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-04-13 22:53:54 +0500

    feat: add global constans

---
+++ src/lib/constants.ts
@@ -0,0 +1,9 @@
+//? Is it actually good
+export const BAR_HEIGHT = 400;
+export const BAR_MIN_HEIGHT = 40;
+
+export const ARRAY_DEFAULT_SIZE = 10
+export const ARRAY_MIN_VALUE = 1
+export const ARRAY_MAX_VALUE = 10 ** 2
+// export const ARRAY_MIN_VALUE = (-10) ** 9
+// export const ARRAY_MAX_VALUE = 10 ** 9
