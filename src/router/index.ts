79993867769da6ce9d4688ac42c15cf42cd90c8c
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-04-19 23:18:17 +0500

    feat: add link to patrh finding page

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createBrowserRouter } from 'react-router'
 
 import App from '../App'
 import SearchVisualizer from '../pages/SearchVisualizer'
+import PathFindingPage from '@/pages/PathFinding'
 
 export const router = createBrowserRouter(
   [
@@ -12,6 +13,10 @@ export const router = createBrowserRouter(
     {
       path: '/visualize/search',
       Component: SearchVisualizer
+    },
+    {
+      path: '/visualize/path-finding',
+      Component: PathFindingPage
     }
   ]
 )
