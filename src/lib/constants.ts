84fa7e115244a07f1dbd99e1698053508935ca82
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-04-19 22:18:01 +0500

    feat: add constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,8 +2,12 @@
 export const BAR_HEIGHT = 400;
 export const BAR_MIN_HEIGHT = 40;
 
+export const MIN_ILLUSTRATION_SPEED = 50 // ms
+export const DEFAULT_ILLUSTRATION_SPEED = 150 // ms
+export const MAX_ILLUSTRATION_SPEED = 1000 // ms
+
 export const ARRAY_DEFAULT_SIZE = 10
 export const ARRAY_MIN_VALUE = 1
-export const ARRAY_MAX_VALUE = 10 ** 2
+export const ARRAY_MAX_VALUE = 10 ** 4
 // export const ARRAY_MIN_VALUE = (-10) ** 9
 // export const ARRAY_MAX_VALUE = 10 ** 9
