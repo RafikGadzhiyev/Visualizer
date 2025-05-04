f2ec1cc47a53e3e29fb5e80a89559acbcf7f61b8
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-05-04 23:47:58 +0500

    feat: update types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,7 +1,22 @@
 export type GridItem = {
   key: string,
+  row: number,
+  col: number,
   blocked: boolean,
   visited: boolean,
-  startPoint: boolean,
-  endPoint: boolean
+  pathPart: boolean,
+  parent?: {
+    row: number,
+    col: number
+  },
+}
+
+export type Position = {
+  row: number,
+  col: number
+}
+
+export type Node = {
+  key: string,
+  parent?: string,
 }
