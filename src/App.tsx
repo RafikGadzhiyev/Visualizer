e8e8f15516d082293a1f64ff946c9bd4010e3db4
Rafik <rafik.gadzhiyev.03@bk.ru>
2025-07-09 16:08:07 +0500

    feat: beautify main page

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,26 @@ import { Link } from "react-router";
 
 function App() {
   return (
-    <div>
+    <div className="h-screen flex flex-col gap-4 items-center justify-center">
+      <div>
+        <h1 className="text-xl font-bold">Visualizer</h1>
+      </div>
+
       <ul>
-        <li>
-          <Link to='/visualize/search'>
+        <li className="mb-2">
+          <Link
+            to='/visualize/search'
+            className="w-full rounded-md p-2 py-1 bg-primary text-black"
+          >
             Search
           </Link>
         </li>
 
         <li>
-          <Link to='/visualize/path-finding'>
+          <Link
+            to='/visualize/path-finding'
+            className="rounded-md p-2 py-1 bg-primary text-black"
+          >
             Path finding
           </Link>
         </li>
